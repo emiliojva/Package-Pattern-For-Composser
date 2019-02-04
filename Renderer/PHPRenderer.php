3c76a48936cbbea1e697c28710af3452814cda61
Emilio Vieira <emiliojva@gmail.com>
2019-02-04 16:14:00 -0200

    Atualizacao de Versao. E retorno de exception na tratameto Renderer

--- Renderer/PHPRenderer.php
+++ Renderer/PHPRenderer.php
@@ -20,18 +20,24 @@ class PHPRenderer implements PHPRendererInterface
 
     public function run()
     {
-        if (is_string($this->data)) {
-            header('Content-type: text/html; charset=UTF-8');
-            echo $this->data;
-            exit;
-        }
+        if(is_null($this->data)) {
+            throw new \Exception('CONTROLLER retornando NULL. Verifique se deu RETURN no $this->render()');
+        } else {
 
-        if (is_array($this->data)) {
-            header('Content-type: application/json');
-            echo json_encode($this->data);
-            exit;
-        }
+            if (is_string($this->data)) {
+                header('Content-type: text/html; charset=UTF-8');
+                echo $this->data;
+                exit;
+            }
 
-        throw new \Exception('Route is invalid');
+            if (is_array($this->data)) {
+                header('Content-type: application/json');
+                echo json_encode($this->data);
+                exit;
+            }
+
+            throw new \Exception('Route is invalid !');
+
+        }
     }
 }
